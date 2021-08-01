a76ecc3560d2ba7969d508f6f402fe400441fdfe
nigelli <nigelli@tencent.com>
2021-08-01 17:33:19 +0800

    base config n .png s

---
+++ CourseCenter/CourseCenter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CourseCenter
+//
+//  Created by nigelli on 2021/8/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
