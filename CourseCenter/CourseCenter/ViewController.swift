21b533e6ce20e0bfe0deb162700dc6536479de57
nigelli <nigelli@tencent.com>
2021-08-03 11:07:38 +0800

    change tab bar item color to black

--- CourseCenter/CourseCenter/ViewController.swift
+++ CourseCenter/CourseCenter/ViewController.swift
@@ -14,16 +14,18 @@ class ViewController: UITabBarController {
         
         let homeViewController = HomeViewController()
         homeViewController.tabBarItem.image = R.image.home()
-        homeViewController.tabBarItem.selectedImage = R.image.home_selected()
+        homeViewController.tabBarItem.selectedImage = R.image.home_selected()?.withRenderingMode(.alwaysOriginal)
         homeViewController.tabBarItem.title = "home"
+        homeViewController.tabBarItem.setTitleTextAttributes([.foregroundColor: UIColor.hexColor(0x333333)], for: .selected)
         
         let navigationHomeViewController = UINavigationController(rootViewController: homeViewController)
         self.addChild(navigationHomeViewController)
         
         let mineViewController = MineViewController()
         mineViewController.tabBarItem.image = R.image.mine()
-        mineViewController.tabBarItem.selectedImage = R.image.mine_selected()
+        mineViewController.tabBarItem.selectedImage = R.image.mine_selected()?.withRenderingMode(.alwaysOriginal)
         mineViewController.tabBarItem.title = "mine"
+        mineViewController.tabBarItem.setTitleTextAttributes([.foregroundColor: UIColor.hexColor(0x333333)], for: .selected)
         
         let navigationMineViewController = UINavigationController(rootViewController: mineViewController)
         self.addChild(navigationMineViewController)
