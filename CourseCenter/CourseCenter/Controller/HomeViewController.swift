9791e810ef10cf1f8a73f323a11458380a740c6d
nigelli <nigelli@tencent.com>
2021-08-03 20:16:53 +0800

    dev banner view with auto scrolling

--- CourseCenter/CourseCenter/Controller/HomeViewController.swift
+++ CourseCenter/CourseCenter/Controller/HomeViewController.swift
@@ -6,24 +6,35 @@
 //
 
 import UIKit
+import Kingfisher
 
-class HomeViewController: BaseViewController {
+class HomeViewController: BaseViewController, BannerViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        
+        let bannerView = BannerView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 180))
+        bannerView.scrollInterval = 3
+        bannerView.dataSource = self
+        view.addSubview(bannerView)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //实现BannerViewDataSource协议方法
+    func numberOfBanners(_ bannerView: BannerView) -> Int {
+        return MockData.createBanners().count
     }
-    */
-
+    
+    func viewForBanner(_ bannerView: BannerView, index: Int, convertView: UIView?) -> UIView {
+        if let view = convertView as? UIImageView {
+            view.kf.setImage(with: URL(string: MockData.createBanners()[index]))
+            return view
+        } else {
+            let imageView = UIImageView()
+            imageView.contentMode = .scaleAspectFit
+            imageView.clipsToBounds = true
+            imageView.kf.setImage(with: URL(string: MockData.createBanners()[index]))
+            return imageView
+        }
+    }
+    
 }
