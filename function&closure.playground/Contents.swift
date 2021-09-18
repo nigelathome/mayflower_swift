a736116a3a7dc543b7ddc278245072df4242e189
Hui Li <nigelathome@126.com>
2021-09-18 17:36:42 +0800

    closure def

--- function&closure.playground/Contents.swift
+++ function&closure.playground/Contents.swift
@@ -1,3 +1,10 @@
+//函数模板
+/*
+ func 函数名(参数签名1 参数1: 参数类型1, 参数签名2 参数2: 参数类型2,...) -> 返回类型 {
+    语句
+ }
+ */
+
 //函数类型
 func addNumber(_ a: Int, _ b: Int) -> Int {
     return a + b
@@ -40,3 +47,34 @@ let stepFunc = updateStepValue(backward: curStep > 0)
 while curStep != 0 {
     curStep = stepFunc(curStep)
 }
+
+//闭包模板
+/*
+ { (参数) -> (返回类型) in
+    语句
+ }
+ */
+
+//闭包sorted(by:)字符串数组逆序
+//传入函数类型、定义闭包、省略闭包类型和返回值、使用形参$0/$1省略in、运算符函数>
+let names = ["zhangsan", "zhaoliu", "lisi", "wangwu"]
+func backward(_ s1: String, _ s2: String) -> Bool {
+    return s1 > s2
+}
+var revNames = names.sorted(by: backward)
+print(revNames)
+
+var revNames2 = names.sorted { (s1: String, s2: String) -> Bool in
+    return s1 > s2
+}
+print(revNames2)
+
+var revName3 = names.sorted(by: { s1, s2 in s1 > s2 } )
+print(revName3)
+
+var revName4 = names.sorted(by: { $0 > $1 } )
+print(revName4)
+
+var revName5 = names.sorted(by: >)
+print(revName5)
+
